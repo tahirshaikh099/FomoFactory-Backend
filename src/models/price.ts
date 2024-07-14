14ea6730d6eb1add8b566018d005f44491b7e3a0
tahirshaikh099 <57958180+tahirshaikh099@users.noreply.github.com>
2024-07-14 14:14:31 +0530

    Delete src directory

--- src/models/price.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from 'mongoose';
-
-const priceSchema = new Schema({
-    symbol: { type: String, required: true },
-    price: { type: Number, required: true },
-    timestamp: { type: Date, default: Date.now }
-});
-
-export const Price = model('Price', priceSchema);
