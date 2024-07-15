7d217aada01fd9638027a90fd4d3dc2cb975aa7a
tahirshaikh099 <57958180+tahirshaikh099@users.noreply.github.com>
2024-07-15 16:32:54 +0530

    Update prices.ts

--- server/src/routes/prices.ts
+++ server/src/routes/prices.ts
@@ -14,25 +14,25 @@ const COINHeader = {
     'X-CoinAPI-Key': `${process.env.COINAPIKEY}`
 };
 
-// async function fetchAndStorePrices() {
-//     console.log("Polling for prices...");
+async function fetchAndStorePrices() {
+    console.log("Polling for prices...");
 
-//     for (const symbol of SYMBOLS) {
-//         try {
-//             const response = await axios.get(`https://rest.coinapi.io/v1/exchangerate/${symbol}/USD`, { headers: COINHeader });
-//             const price = new Price({ symbol, price: response.data.rate });
-//             await price.save();
-//             console.log(`Saved price for ${symbol}: ${response.data.rate}`);
-//         } catch (error) {
-//             console.error(`Error fetching price for ${symbol}:`, error);
-//         }
-//     }
-//     // Schedule the next polling after the current one finishes
-//     setTimeout(fetchAndStorePrices, 864000); // 14 minutes and 24 seconds
-// }
+    for (const symbol of SYMBOLS) {
+        try {
+            const response = await axios.get(`https://rest.coinapi.io/v1/exchangerate/${symbol}/USD`, { headers: COINHeader });
+            const price = new Price({ symbol, price: response.data.rate });
+            await price.save();
+            console.log(`Saved price for ${symbol}: ${response.data.rate}`);
+        } catch (error) {
+            console.error(`Error fetching price for ${symbol}:`, error);
+        }
+    }
+    // Schedule the next polling after the current one finishes
+    setTimeout(fetchAndStorePrices, 864000); // 14 minutes and 24 seconds
+}
 
-// // Initial call to start polling
-// fetchAndStorePrices();
+// Initial call to start polling
+fetchAndStorePrices();
 
 router.get('/:symbol', async (req, res) => {
     const { symbol } = req.params;
